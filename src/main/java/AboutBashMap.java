e81a72dd2568a2ec80ab2b38830c17c7766ece45
iphonexsmax25 <limisaac2022@gmail.com>
2026-07-20 17:57:31 +0800

    works

--- src/main/java/AboutBashMap.java
+++ src/main/java/AboutBashMap.java
@@ -5,7 +5,22 @@ public class AboutBashMap {
         test1();
     }
     public static void test1(){
-        
+        HashMap<String, Integer> enrolment = new HashMap<>();
+        enrolment.put("CSIT121", 280);
+        enrolment.put("CSIT128", 150);
+        enrolment.put("CSIT213", 250);
+        enrolment.put("CSIT121", 300); // replace original value
+        // Accessing HashMap
+        for(String modCode : enrolment.keySet()){
+            System.out.println(modCode);
+        }
+        for(int classSize : enrolment.values()){
+            System.out.println(classSize);
+        }
+        // USe th key to get the value
+        for(String modCode : enrolment.keySet()){
+            System.out.println(modCode + " " + enrolment.get(modCode));
+        }
     }
     
 }
