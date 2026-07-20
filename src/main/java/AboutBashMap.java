b6b0bd11c077c563e19cfb38949220b0396e4bba
iphonexsmax25 <limisaac2022@gmail.com>
2026-07-20 17:46:51 +0800

    works

--- src/main/java/AboutBashMap.java
+++ src/main/java/AboutBashMap.java
@@ -1,5 +1,11 @@
-
+import java.util.HashMap;
 
 public class AboutBashMap {
+    public static void main (String[] args){
+        test1();
+    }
+    public static void test1(){
+        
+    }
     
 }
